53e172a1806948352a1d4c83dddbfbee1357858a
Osamu Takahashi <woolist@hotmail.com>
2016-06-25 07:34:54 +0900

    バイトをクライアントから読み込んで表示するだけのプログラム

--- src/test/net/OutputStreamClient.java
+++ src/test/net/OutputStreamClient.java
@@ -1,9 +1,39 @@
 package test.net;
 
-public class OutputStreamClient {
-	private final static int SERVER_PORT = 65143;
-	public static void main(String[] args) {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
+public class OutputStreamClient {
+	private final static int SERVER_PORT = 65333;
+	private final static String SERVER_ADDRESS = "localhost";
+	public static void main(String[] args) {
+		Socket socket = null;
+		OutputStream os = null;
+		try {
+			socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
+			os = socket.getOutputStream();
+			os.write(10);
+			os.flush();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(os != null) {
+				try {
+					os.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if(socket != null) {
+				try {
+					socket.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
--- src/test/net/SimpleServer.java
+++ src/test/net/SimpleServer.java
@@ -1,9 +1,42 @@
 package test.net;
 
-public class SimpleServer {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class SimpleServer {
+	private static final int SERVER_PORT = 65333;
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		InputStream is = null;
+		try {
+			serverSocket = new ServerSocket(SERVER_PORT);
+			socket = serverSocket.accept();
+			is = socket.getInputStream();
+			byte[] buf = new byte[100];
+			is.read(buf);
+			System.out.println(buf[0]);
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(is != null) {
+				try {
+					is.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if(socket != null) {
+				try {
+					socket.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
