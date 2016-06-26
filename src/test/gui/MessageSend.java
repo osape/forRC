1d590a2ce90598cfa914b028c71e7294bccebdd3
Osamu Takahashi <woolist@hotmail.com>
2016-06-26 23:15:52 +0900

    GUIでチャットをできるようにした。

---
+++ src/test/gui/MessageSend.java
@@ -0,0 +1,34 @@
+package test.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+import test.net.OutputStreamClient;
+
+public class MessageSend implements ActionListener {
+	/**
+	 * メッセージ出力サーバーポート
+	 */
+	private final int PORT;
+
+	/**
+	 * サーバホスト名
+	 */
+	private final String ADDRESS;
+
+	public MessageSend(int OUTPUT_SERVER_PORT,String SERVER_ADDRESS) {
+		PORT = OUTPUT_SERVER_PORT;
+		ADDRESS = SERVER_ADDRESS;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		OutputStreamClient osc = new OutputStreamClient(PORT, ADDRESS);
+		JTextField jt = (JTextField)e.getSource();
+		osc.output(jt.getText());
+		jt.setText("");
+	}
+
+}
