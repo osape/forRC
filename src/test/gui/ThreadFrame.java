f9676e38d3e3ceb82c67423107c67a7fbcf247bc
osamu <osamu@osamu-vaio>
2016-06-27 14:52:25 +0900

    ThreadFrameの変更

--- src/test/gui/ThreadFrame.java
+++ src/test/gui/ThreadFrame.java
@@ -1,6 +1,7 @@
 package test.gui;
 
 import java.awt.Container;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -11,15 +12,18 @@ public class ThreadFrame extends JFrame {
 	private JTextArea sendMessage;
 
 	public ThreadFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		receiveMessage = new JTextArea();
 		JScrollPane jscRec = new JScrollPane(receiveMessage);
 		sendMessage = new JTextArea();
 		JScrollPane jscSen = new JScrollPane(sendMessage);
 
 		Container container = getContentPane();
+		container.setLayout(new GridLayout(2, 1));
 		container.add(jscRec);
 		container.add(jscSen);
 
+		pack();
 		setVisible(true);
 	}
 }
