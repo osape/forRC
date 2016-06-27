378d462383bcd12ad3f7aa5fc99b494993004360
osamu <osamu@osamu-vaio>
2016-06-27 15:44:43 +0900

    GUIClientにコメントを追加

--- src/test/gui/GUIClient.java
+++ src/test/gui/GUIClient.java
@@ -1,5 +1,13 @@
 package test.gui;
 
+/**
+ * スレッド管理版のプログラム
+ * 整数のデータを送信する
+ * 整数のデータを受信する、受信するデータはすべてのクライアントから送信されたデータを受信する
+ *
+ * @author osamu
+ *
+ */
 public class GUIClient {
 
 	public static void main(String[] args) {
