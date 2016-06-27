63914a3b24034e0d9ca7ad2b6d80757eb87533d1
osamu <osamu@osamu-vaio>
2016-06-27 13:31:10 +0900

    GUIクラスの追加

--- src/test/gui/ThreadFrame.java
+++ src/test/gui/ThreadFrame.java
@@ -1,5 +1,25 @@
 package test.gui;
 
-public class ThreadFrame {
+import java.awt.Container;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class ThreadFrame extends JFrame {
+	private JTextArea receiveMessage;
+	private JTextArea sendMessage;
+
+	public ThreadFrame() {
+		receiveMessage = new JTextArea();
+		JScrollPane jscRec = new JScrollPane(receiveMessage);
+		sendMessage = new JTextArea();
+		JScrollPane jscSen = new JScrollPane(sendMessage);
+
+		Container container = getContentPane();
+		container.add(jscRec);
+		container.add(jscSen);
+
+		setVisible(true);
+	}
 }
