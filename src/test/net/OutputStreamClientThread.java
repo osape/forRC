4f5223e75fc4682de3b85236e7a35645a12d2795
osamu <osamu@osamu-vaio>
2016-06-27 12:09:28 +0900

    メッセージのやりとりにスレッドを利用する

--- src/test/net/OutputStreamClientThread.java
+++ src/test/net/OutputStreamClientThread.java
@@ -42,7 +42,6 @@ public class OutputStreamClientThread extends Thread {
 		Socket socket = null;
 
 		OutputStream os = null;
-		byte[] buf = { 1,2,3,4,5,6,7,8,9,10 };
 		try {
 			for(int i = 0;i < 100;i++) {
 				socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
