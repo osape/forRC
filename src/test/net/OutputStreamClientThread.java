34d4913e1b3c96d8c5ec905b3ca02ebc2ca8dbfb
osamu <osamu@osamu-vaio>
2016-07-01 09:29:08 +0900

    キーボード入力 チャット

--- src/test/net/OutputStreamClientThread.java
+++ src/test/net/OutputStreamClientThread.java
@@ -38,57 +38,32 @@ public class OutputStreamClientThread extends Thread {
 	 */
 	@Override
 	public void run() {
-		/**
-		 * サーバソケット
-		 */
-		Socket socket = null;
-
-		OutputStream os = null;
-		BufferedReader br = null;
-
-		try {
-			br = new BufferedReader(new InputStreamReader(System.in));
+		try (
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			)
+		{
 			for(int i = 0;i < 100;i++) {
 				System.out.print("メッセージ入力 : ");
 				String input = br.readLine();
-
-				socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
-				os = socket.getOutputStream();
-				String message = socket.getInetAddress().getHostName() + " : " + i + " : " + input;
-				byte[] buf = message.getBytes("UTF-8");
-				os.write(buf);
-				os.flush();
-				os.close();
+				try (
+						Socket socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
+						OutputStream os = socket.getOutputStream();
+					)
+				{
+					String message = socket.getInetAddress().getHostName() + " : " + i + " : " + input;
+					byte[] buf = message.getBytes("UTF-8");
+					os.write(buf);
+					os.flush();
+					os.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
 				sleep(1000);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		} catch(InterruptedException e) {
 			e.printStackTrace();
-		} finally {
-			if(os != null) {
-				try {
-					os.close();
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			if(br != null) {
-				try {
-					br.close();
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			if(socket != null) {
-				try {
-					socket.close();
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
